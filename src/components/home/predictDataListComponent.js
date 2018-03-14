38f0441d0e8d37aac785dfbed72ad454bd7925b9
Hieu Ta <hieu.ta@hubcba.com>
2018-03-15 03:41:58 +1100

    support dynamic render

--- src/components/home/predictDataListComponent.js
+++ src/components/home/predictDataListComponent.js
@@ -4,33 +4,36 @@ import {connect} from 'react-redux'
 const mapStateToProps = (state, props) => {
     let predictData = state.predictData;
     let predictDataList = predictData.dataList;
+    let predictDataFirst = predictDataList && predictDataList.length > 0 ? predictDataList[0] : {}
+    let predictKeyList  = [];
+    for (var key in predictDataFirst) {
+        if (predictDataFirst.hasOwnProperty(key)) {
+            predictKeyList.push(key);
+        }
+    };
     return {
+        predictKeyList : predictKeyList,
         predictDataList: predictDataList || []
     }
 }
 
-const PredictionDataList = ({predictDataList}) => {
-    console.log("value" + predictDataList.length)
+const PredictionDataList = ({predictKeyList, predictDataList}) => {
     return (
-            <div className="row margin-bottom-10 margin-top-30">
+            <div className="row margin-bottom-10 margin-top-30" style={{overflowX: 'scroll'}}>
                 <table className="table table-striped">
                     <thead>
-                    {predictDataList.length > 0 &&
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Age</th>
-                        <th scope="col">Occupation</th>
-                        <th scope="col">Fraud</th>
+                        {predictKeyList.length > 0 && predictKeyList.map((key) => {
+                            return <th scope="col">{key}</th>
+                        })}
                     </tr>
-                    }
                     </thead>
                     <tbody>
                     {predictDataList.length > 0 && predictDataList.map((item, index) => (
                         <tr key={index} style={{color : item.fraud == 1 && 'red'}}>
-                            <th scope="row">{++index}</th>
-                            <td>{item.age}</td>
-                            <td>{item.occupation}</td>
-                            <td>{item.fraud}</td>
+                            {predictKeyList.map((key) => {
+                                return <td>{item[key]}</td>
+                            })}
                         </tr>
                     ))}
                     </tbody>
