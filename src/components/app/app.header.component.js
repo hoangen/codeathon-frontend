6af6b66321230eaa8fc907b738adcb4833fd6740
Hieu Ta <hieu.ta@hubcba.com>
2018-03-15 04:11:17 +1100

    Support login

--- src/components/app/app.header.component.js
+++ src/components/app/app.header.component.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import logo from '../../assets/images/logo.svg'
 import '../../assets/css/common.css'
+import {connect} from "react-redux";
 
-const Header = () => (
+
+const mapStateToProps = (state, props) => {
+    let form = state.form;
+    let loginForm = form.loginInformation;
+    let values = loginForm && loginForm.values;
+    let username = (values && values.username);
+    return {
+        userName: username
+    }
+}
+
+
+const Header = ({userName}) => (
     <div id="header">
         <div className="container">
             <a href="/" className="Logo"><img src={logo} alt="Logo" /></a>
-            <a href="/login" className="login" style={{float:'right'}}>Login</a>
+            {!userName &&
+                <a href="/login" className="login" style={{float:'right', color: '#337ab7'}}>Login</a>
+            }
+            {userName &&
+                <p className="login" style={{float: 'right'}}>Hello {userName}</p>
+            }
         </div>
     </div>
 )
 
-export default Header
+export default connect(mapStateToProps)(Header)
+
